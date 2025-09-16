a00063e97ec3a93dbb5c914afd24d1310634185e
Yogita Sharma <168211862+YOGITASHARMAAA@users.noreply.github.com>
2025-09-16 13:41:33 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version.
+
+console.log(3 + 
+    4)    //writing a code like this is valid but the readability should be high so we should not write like this way.
+
+//alert( 3 + 3 )  we are using node js, not browser
+
+
+//DataTypes!!
+let name = "Yogita Sharma"
+let age = 24
+let isLoggedIn = true
+
+//Primitive DATATYPES---
+// number - range => 2 to power 
+// bigint
+// string => " "
+// boolean => true/false 
+// null => standalone value
+// undefined => value not assinged
+// symbol => unique
+
+// object--
+
+console.log(typeof undefined);   //undefined
+
+console.log(typeof null)   //Object
