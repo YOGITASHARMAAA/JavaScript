1671b20c2735b51729eb48e002f56bfe4572edbd
Yogita Sharma <168211862+YOGITASHARMAAA@users.noreply.github.com>
2025-09-17 14:58:09 +0000

    conversion

--- basics/conversionOrOperations.js
+++ basics/conversionOrOperations.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
+//console.log(typeof score);
 //console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => easily got coverted in number - 33
@@ -15,8 +15,8 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -26,5 +26,61 @@ console.log(booleanIsLoggedIn)
 let someNumber = 34
 
 let valueInString = String(someNumber)
-console.log(typeof valueInString)
-console.log(valueInString)
+// console.log(typeof valueInString)
+// console.log(valueInString)
+
+
+// Why string to number conversion is confusing!!
+//*************operations********* 
+
+let value = 3
+let negValue = -value
+console.log(negValue);   // -3
+
+// basics operations----
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " Yogita"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+// if string first then every other after that treated as a string only and if the string is in last then first the number get add and then string get attached to it.  
+
+
+// console.log("1" + 2);   //12
+// console.log(1 + "2");   //12  
+// console.log(1 + "2" + 1);   //121
+// console.log("1" + 2 + 2);   //122
+// console.log(1 + 2 + "2"); //32
+
+
+// console.log(3 + 4 * 5 % 3); not right way to write the code
+
+
+
+// special type of conversion--
+
+
+// console.log(+true);  should not do this 
+// console.log(+"");
+
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2  should not do this 
+
+
+// prefix and postfix
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
